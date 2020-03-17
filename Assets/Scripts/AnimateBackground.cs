79b28bd1f0c7653d8c1d7cec49988a6611870751
Peti <meti651@gmail.com>
2020-03-17 22:46:22 +0100

    main menu + base scene structure

---
+++ Assets/Scripts/AnimateBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimateBackground : MonoBehaviour
+{
+    [SerializeField]Sprite[] animatedImages;
+    [SerializeField] Image animateImage;
+    int framesPerSecond = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int index = (int)(Time.time * framesPerSecond) % animatedImages.Length;
+        animateImage.sprite = animatedImages[index];
+    }
+}
---
+++ Assets/Scripts/MainMenuButtonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonController : MonoBehaviour
+{
+    [SerializeField] string PlayGameName;
+    [SerializeField] string VisualizationName;
+    [SerializeField] string PathFinderName;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(PlayGameName);
+    }
+
+    public void VisualizeMazeGenerator()
+    {
+        SceneManager.LoadScene(VisualizationName);
+    }
+
+    public void PathFinder()
+    {
+        SceneManager.LoadScene(PathFinderName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
