0f73b8829c4dc032ef12179e675fe5cb357b9a74
Peti <meti651@gmail.com>
2020-03-19 19:10:25 +0100

    finishing game

---
+++ Assets/Scripts/FinishConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishConfig : MonoBehaviour
+{
+    public GameObject cell;
+    public GameObject maze;
+
+    void Start()
+    {
+        float wallLenght = cell.GetComponent<CellConstruct>().wallLenght;
+        BoxCollider finishTrigger = this.GetComponent<BoxCollider>();
+        finishTrigger.enabled = true;
+        finishTrigger.isTrigger = true;
+        finishTrigger.size = new Vector3(wallLenght, wallLenght, wallLenght);
+
+
+        MazeModel mazeComponent = maze.GetComponent<MazeModel>();
+
+        this.transform.position = new Vector3((mazeComponent.row - 1) * wallLenght, 0f, (mazeComponent.column - 1) * wallLenght);
+    }
+}
