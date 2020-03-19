3945210593bbc4200a234d668c825ef4a8207716
Peti <meti651@gmail.com>
2020-03-19 19:11:59 +0100

    pause menu functionality

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public bool GameIsPaused = false;
+
+    public GameObject PauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void QuitToMainMenu()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
