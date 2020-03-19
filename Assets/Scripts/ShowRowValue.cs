6577d588de44b51d18c622966f998c62e56d6e19
Peti <meti651@gmail.com>
2020-03-19 19:13:48 +0100

    start menu functionality

---
+++ Assets/Scripts/ShowRowValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowRowValue : MonoBehaviour
+{
+    TextMeshProUGUI rowNum;
+
+    private void Start()
+    {
+        rowNum = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void ChangeText(float value)
+    {
+        rowNum.text = Mathf.RoundToInt(value).ToString();
+    }
+}
---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StartMenuController : MonoBehaviour
+{
+    public GameObject Maze;
+    public TextMeshProUGUI rowNum;
+    public TextMeshProUGUI columnNum;
+    public GameObject StartMenu;
+
+    public void SetMazeActive()
+    {
+        int row = Int32.Parse(rowNum.text);
+        int column = Int32.Parse(columnNum.text);
+
+        MazeModel maze = Maze.GetComponent<MazeModel>();
+        maze.row = row;
+        maze.column = column;
+
+        StartMenu.SetActive(false);
+        Maze.SetActive(true);
+    }
+
+}
